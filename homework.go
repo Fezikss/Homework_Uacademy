9b7112ac184975ea8829dd66f47ac69e0581cc99
Fezikss <fozilbek.info@gmail.com>
2023-11-18 03:08:48 +0500

    i did all homework

---
+++ homework.go
@@ -0,0 +1,91 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 1
+	// var son int
+	// fmt.Print("Uch xonali sonni kiriting: ")
+	// fmt.Scanln(&son)
+
+	// o := son / 10
+	// b := son % 10
+	// almashtirilganSon := b*10 + o
+	// fmt.Println("Natija:", almashtirilganSon)
+
+	//2
+	// var son int
+	// fmt.Print("Sonni kiriting: ")
+	// fmt.Scanln(&son)
+
+	// sonButun := son / 10
+	// sonQoldiq := son % 10
+	// y := sonButun % 100
+	// fmt.Println("Natija:", y)
+
+	//3
+	// var son int
+	// fmt.Print("Sonni kiriting: ")
+	// fmt.Scanln(&son)
+
+	// sonButun := son / 10
+	// sonQoldiq := son % 10
+	// m := sonButun % 1000
+	// fmt.Println("Natija:", m)
+
+	//4
+	// var sekund int
+	// fmt.Print("Kun boshidan boshlab o'tgan sekundlarni kiriting: ")
+	// fmt.Scanln(&sekund)
+
+	// minut := sekund / 60
+	// fmt.Println("Kun boshidan boshlab", minut, "minut o'tgan")
+
+	//5
+	// var sekund int
+	// fmt.Print("Kun boshidan boshlab o'tgan sekundlarni kiriting: ")
+	// fmt.Scanln(&sekund)
+
+	// soat := sekund / 3600
+	// fmt.Println("Kun boshidan boshlab", soat, "soat o'tgan")
+
+	//6
+	// var sekund int
+	// fmt.Print("Kun boshidan boshlab o'tgan sekundlarni kiriting: ")
+	// fmt.Scanln(&sekund)
+
+	// minut := sekund / 60
+	// sekund = sekund % 60
+	// fmt.Println("Kun boshidan boshlab", minut, "minut", sekund, "sekund o'tgan")
+
+	//7
+	// var sekund int
+	// fmt.Print("Kun boshidan boshlab o'tgan sekundlarni kiriting: ")
+	// fmt.Scanln(&sekund)
+	// soat := sekund / 3600
+	// sekund = sekund % 3600
+	// fmt.Println("Kun boshidan boshlab", soat, "soat", sekund, "sekund o'tgan")
+
+	//8
+	// var sekund int
+	// fmt.Print("Kun boshidan boshlab o'tgan sekundlarni kiriting: ")
+	// fmt.Scanln(&sekund)
+
+	// soat := sekund / 3600
+	// sekund = sekund % 3600
+	// minut := sekund / 60
+	// sekund = sekund % 60
+	// fmt.Println("Kun boshidan boshlab", soat, "soat", minut, "minut", sekund, "sekund o'tgan")
+
+	//9
+	// var son int
+	// fmt.Print("Sonni kiriting: ")
+	// fmt.Scanln(&son)
+
+	// sonButun := son / 10
+	// sonQoldiq := son % 10
+	// m := sonButun % 1000
+	// fmt.Println("Natija:", m)
+
+}
