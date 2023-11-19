8716317398198caabb05337127bc1df9dd3e7a6b
Fozilbek <120035130+Fezikss@users.noreply.github.com>
2023-11-19 20:25:12 +0500

    Add files via upload

---
+++ proctec.go
@@ -0,0 +1,410 @@
+package main
+
+func main() {
+	//1
+	// var (
+	// 	n int
+	// )
+	// fmt.Print("n : ")
+	// fmt.Scan(&n)
+	// if n%3 == 0 && n%5 == 0 {
+	// 	fmt.Println("FizzBUZZ")
+	// } else if n%3 == 0 {
+	// 	fmt.Println("Fizz")
+	// } else if n%5 == 0 {
+	// 	fmt.Println("BUZZ")
+	// }
+	//2
+	// var (
+	// 	n int
+	// )
+	// fmt.Print("n : ")
+	// fmt.Scan(&n)
+
+	// if n%4 == 0 && n%100 != 0 {
+	// 	fmt.Println("Kabisa !")
+	// } else {
+	// 	fmt.Println("Kabisa emas !")
+	// }
+	//3
+	// var (
+	// 	n int
+	// )
+	// fmt.Print("n : ")
+	// fmt.Scan(&n)
+
+	// if n%2 == 0 {
+	// 	fmt.Println(n * n)
+	// } else if n%2 == 1 {
+	// 	fmt.Println(n * n * n)
+	// }
+
+	//4
+	// var (
+	// 	age int
+	// )
+	// fmt.Print("yoshingizni kiriting : ")
+	// fmt.Scan(&age)
+
+	// if age % 2 == 0 {
+	// 	fmt.Println("Toxirov")
+	// }else if age % 2 == 1 {
+	// 	fmt.Println("Fozilbek")
+	// }
+
+	//5
+	// var (
+	// 	n int
+	// )
+	// fmt.Print("n = ")
+	// fmt.Scan(&n)
+
+	// if n > 0 {
+	// 	n++
+	// 	fmt.Println(n)
+	// } else if n < 0 {
+	// 	fmt.Println(n)
+	// } else {
+	// 	fmt.Println(n, " musbatham manfiyham emas !")
+	// }
+
+	//6
+	// var (
+	// 	n int
+	// )
+	// fmt.Print("n = ")
+	// fmt.Scan(&n)
+
+	// if n > 0 {
+	// 	n++
+	// 	fmt.Println(n)
+	// } else if n < 0 {
+	// 	n -= 2
+	// 	fmt.Println(n)
+	// } else {
+	// 	fmt.Println(n, " musbatham manfiyham emas !")
+	// }
+
+	//7
+	// var (
+	// 	n int
+	// )
+	// fmt.Print("n = ")
+	// fmt.Scan(&n)
+
+	// if n > 0 {
+	// 	n++
+	// 	fmt.Println(n)
+	// } else if n < 0 {
+	// 	n -= 2
+	// 	fmt.Println(n)
+	// } else {
+	// 	n = 10
+	// 	fmt.Println(n)
+	// }
+
+	//8
+	// var (
+	// 	a,b,c int
+	// 	counter int
+	// )
+	// fmt.Print("a,b,c : ")
+	// fmt.Scan(&a,&b,&c)
+
+	// if a > 0 {
+	// 	counter++
+	// }
+	// if b > 0 {
+	// 	counter++
+	// }
+	// if c > 0 {
+	// 	counter++
+	// }
+
+	// fmt.Print(counter," ta musbat son bor ! ")
+
+	//9
+	// var (
+	// 	a,b,c int
+	// 	counter = 0
+	// 	counterMinus = 0
+	// )
+	// fmt.Print("a,b,c : ")
+	// fmt.Scan(&a,&b,&c)
+
+	// if a > 0 {
+	// 	counter++
+	// }
+	// if b > 0 {
+	// 	counter++
+	// }
+	// if c > 0 {
+	// 	counter++
+	// }
+	// if a < 0 {
+	// 	counterMinus++
+	// }
+	// if b < 0 {
+	// 	counterMinus++
+	// }
+	// if c < 0 {
+	// 	counterMinus++
+	// }
+
+	// fmt.Print(counter," ta musbat son bor ! ")
+	// fmt.Print(counterMinus," ta manfiy son bor ! ")
+	//10
+	// var (
+	// 	a,b int
+	// )
+	// fmt.Print("a, b : ")
+	// fmt.Scan(&a,&b)
+
+	// if a > b{
+	// 	fmt.Println(a ," is greater than ",b)
+	// }else {
+	// 	fmt.Println(b ," is greater than ",a)
+	// }
+	//11
+	// var (
+	// 	a,b int
+	// )
+	// fmt.Print("a, b : ")
+	// fmt.Scan(&a,&b)
+
+	// if a > b{
+	// 	fmt.Println(b)
+	// }else {
+	// 	fmt.Println(a)
+	// }
+
+	//12
+	// var (
+	// 	a,b int
+	// )
+	// fmt.Print("a, b : ")
+	// fmt.Scan(&a,&b)
+
+	// if a > b{
+	// 	fmt.Println(a ,b)
+	// }else {
+	// 	fmt.Println(b ,a)
+	// }
+
+	//                       for
+
+	//1
+	// var (
+	// 	k int
+	// 	n int
+	// )
+	// fmt.Print("k,n = ")
+	// fmt.Scan(&k, &n)
+
+	// for i := 0; i < n; i++ {
+	// 	fmt.Println(k)
+	// }
+
+	//2
+	// var (
+	// 	a       int
+	// 	b       int
+	// 	counter = 0
+	// )
+	// fmt.Print("a,b = ")
+	// fmt.Scan(&a, &b)
+
+	// for i := a; i <= b; i++ {
+	// 	counter++
+	// 	fmt.Println(i)
+	// }
+
+	// fmt.Println()
+	// fmt.Println(counter, " ta son chiqdi ! ")
+
+	//3
+	// var (
+	// 	a       int
+	// 	b       int
+	// 	counter = 0
+	// )
+	// fmt.Print("a,b = ")
+	// fmt.Scan(&a, &b)
+
+	// for i := b - 1; a < i; i-- {
+	// 	counter++
+	// 	fmt.Println(i)
+	// }
+
+	// fmt.Println()
+	// fmt.Println(counter, " ta son chiqdi ! ")\
+
+	// 4
+
+	// var (
+	// 	kanfetNarxi float32
+	// 	sum         float32
+	// )
+	// fmt.Print("Kanfet narxini kiriting : ")
+	// fmt.Scan(&kanfetNarxi)
+
+	// for i := 1; i <= 10; i++ {
+	// 	sum = kanfetNarxi * float32(i)
+	// 	fmt.Println(i, "kg kanfet narxi : ", sum)
+
+	// }
+	//5
+	// var (
+	// 	kanfetNarxi float32
+	// 	sum         float32
+	// )
+	// fmt.Print("Kanfet narxini kiriting : ")
+	// fmt.Scan(&kanfetNarxi)
+
+	// for i := 1; i <= 10; i++ {
+	// 	sum = (kanfetNarxi * float32(i)) * 0.1
+	// 	// fmt.Println(float32(i)*0.1, "kg kanfet narxi : ", sum)
+	// 	fmt.Printf("%.3f kg kanfet narxi = %.2f\n", float32(i)*0.1, sum)
+	// }
+
+	//            switch
+
+	//1
+
+	// var (
+	// 	haftakunlari int
+	// )
+	// fmt.Print("Hafta kunini kiriting: ")
+	// fmt.Scan(&haftakunlari)
+
+	// switch haftakunlari {
+	// case 1:
+	// 	fmt.Println("Dushanba")
+	// case 2:
+	// 	fmt.Println("Seshanba")
+	// case 3:
+	// 	fmt.Println("Chorshanba")
+	// case 4:
+	// 	fmt.Println("Payshanba")
+	// case 5:
+	// 	fmt.Println("Juma")
+	// case 6:
+	// 	fmt.Println("Shanba")
+	// case 7:
+	// 	fmt.Println("Yakshanba")
+	// default:
+	// 	fmt.Println("Bunday hafta kuni mavjud emas !")
+	// }
+
+	//2
+
+	// var (
+	// 	K int
+	// )
+	// fmt.Print("Baxo qoying : ")
+	// fmt.Scan(&K)
+
+	// switch K {
+	// case 1: fmt.Println("Yomon")
+	// case 2: fmt.Println("Qoniqarsiz")
+	// case 3: fmt.Println("Qoniqarliy")
+	// case 4: fmt.Println("Yaxshi")
+	// case 5: fmt.Println("a'lo")
+
+	// default: fmt.Println("Xato")
+
+	// }
+
+	//3
+
+	// var (
+	// 	monthNumber int
+	// )
+	// fmt.Print("Oy raqamini kiriting : ")
+	// fmt.Scan(&monthNumber)
+
+	// switch monthNumber {
+	// case 1,2,3: fmt.Println("Qish")
+	// case 4,5,6: fmt.Println("Bahor")
+	// case 7,8,9: fmt.Println("Yoz")
+	// case 10,11,12: fmt.Println("Kuz")
+
+	// default: fmt.Println("Xato")
+	//}
+
+	//4
+
+	// var (
+	// 	monthNumber int
+	// )
+	// fmt.Print("Oy raqamini kiriting : ")
+	// fmt.Scan(&monthNumber)
+
+	// switch monthNumber {
+	// case 1:
+	// 	fmt.Println("30 kun Bor !")
+	// case 2:
+	// 	fmt.Println("28 kun Bor !")
+	// case 3:
+	// 	fmt.Println("31 kun Bor !")
+	// case 4:
+	// 	fmt.Println("30 kun Bor !")
+	// case 5:
+	// 	fmt.Println("31 kun Bor !")
+	// case 6:
+	// 	fmt.Println("30 kun Bor !")
+	// case 7:
+	// 	fmt.Println("31 kun Bor !")
+	// case 8:
+	// 	fmt.Println("30 kun Bor !")
+	// case 9:
+	// 	fmt.Println("31 kun Bor !")
+	// case 10:
+	// 	fmt.Println("30 kun Bor !")
+	// case 11:
+	// 	fmt.Println("31 kun Bor !")
+	// case 12:
+	// 	fmt.Println("31 kun Bor !")
+
+	// default: fmt.Println("Xato 1")
+	// }
+
+	//5
+	// var (
+	// 	a           float32
+	// 	b           float32
+	// 	conculation int
+	// )
+
+	// fmt.Print("a : ")
+	// fmt.Scan(&a)
+	// fmt.Print("b : ")
+	// fmt.Scan(&b)
+	// fmt.Println()
+
+	// for true {
+	// 	fmt.Print("1 => + , 2 =>  - , 3 => / , 4 => * , 5 => EXIT  : ")
+	// 	fmt.Scan(&conculation)
+	// 	fmt.Println()
+	// 	if conculation == 5 {
+	// 		break
+	// 	}
+
+	// 	switch conculation {
+	// 	case 1:
+	// 		fmt.Println(a, " + ", b, " = ", a+b)
+	// 	case 2:
+	// 		fmt.Println(a, " - ", b, " = ", a-b)
+	// 	case 3:
+	// 		fmt.Println(a, " / ", b, " = ", a/b)
+	// 	case 4:
+	// 		fmt.Println(a, " * ", b, " = ", a*b)
+
+	// 	default:
+	// 		fmt.Println("Iltimos 1 - 4 gacham ixtiyoriy raqm kiriting !")
+
+	// 	}
+	// 	fmt.Println()
+	// }
+
+}
