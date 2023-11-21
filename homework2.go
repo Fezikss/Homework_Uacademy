60c94003fa347bebc19b484aa024defe3659d802
Fozilbek <120035130+Fezikss@users.noreply.github.com>
2023-11-21 15:26:51 +0500

    homework  for  "if,switch for loop "

---
+++ homework2.go
@@ -0,0 +1,521 @@
+package main
+
+func main() {
+	//if
+
+	//1
+
+	// var A, B float64
+
+	// fmt.Print("A ni kiriting: ")
+	// fmt.Scan(&A)
+
+	// fmt.Print("B ni kiriting: ")
+	// fmt.Scan(&B)
+
+	// if A > B {
+	// 	A, B = B, A
+	// }
+
+	// fmt.Println("A =", A)
+	// fmt.Println("B =", B)
+
+	//2
+
+	// var A, B int
+
+	// fmt.Print("A ni kiriting: ")
+	// fmt.Scan(&A)
+
+	// fmt.Print("B ni kiriting: ")
+	// fmt.Scan(&B)
+
+	// if A != B {
+	// 	A = A + B
+	// 	B = A
+	// } else {
+	// 	A = 0
+	// 	B = 0
+	// }
+
+	// fmt.Println("A =", A)
+	// fmt.Println("B =", B)
+
+	//3
+
+	// var A, B int
+
+	// fmt.Print("A ni kiriting: ")
+	// fmt.Scan(&A)
+
+	// fmt.Print("B ni kiriting: ")
+	// fmt.Scan(&B)
+
+	// if A != B {
+	// 	if A > B {
+	// 		A = 0
+	// 	} else {
+	// 		B = 0
+	// 	}
+	// } else {
+	// 	A = 0
+	// 	B = 0
+	// }
+
+	// fmt.Println("A =", A)
+	// fmt.Println("B =", B)
+
+	//4
+
+	// var num1, num2, num3 int
+
+	// fmt.Print("1-sonni kiriting: ")
+	// fmt.Scan(&num1)
+
+	// fmt.Print("2-sonni kiriting: ")
+	// fmt.Scan(&num2)
+
+	// fmt.Print("3-sonni kiriting: ")
+	// fmt.Scan(&num3)
+
+	// min := num1
+
+	// if num2 < min {
+	// 	min = num2
+	// }
+
+	//5
+
+	// var num1, num2, num3 int
+
+	// fmt.Print("1-sonni kiriting: ")
+	// fmt.Scan(&num1)
+
+	// fmt.Print("2-sonni kiriting: ")
+	// fmt.Scan(&num2)
+
+	// fmt.Print("3-sonni kiriting: ")
+	// fmt.Scan(&num3)
+
+	// max := num1
+	// min := num1
+
+	// if num2 > max {
+	// 	max = num2
+	// }
+
+	// if num3 > max {
+	// 	max = num3
+	// }
+
+	// if num2 < min {
+	// 	min = num2
+	// }
+
+	// if num3 < min {
+	// 	min = num3
+	// }
+
+	// average := (max + min) / 2
+
+	// fmt.Println("O'rta son:", average)
+
+	//6
+
+	// var num1, num2, num3 int
+
+	// fmt.Print("1-sonni kiriting: ")
+	// fmt.Scan(&num1)
+
+	// fmt.Print("2-sonni kiriting: ")
+	// fmt.Scan(&num2)
+
+	// fmt.Print("3-sonni kiriting: ")
+	// fmt.Scan(&num3)
+
+	// min := num1
+	// max := num1
+
+	// if num2 < min {
+	// 	min = num2
+	// }
+
+	// if num3 < min {
+	// 	min = num3
+	// }
+
+	// if num2 > max {
+	// 	max = num2
+	// }
+
+	// if num3 > max {
+	// 	max = num3
+	// }
+
+	// fmt.Println("Kichik son:", min)
+	// fmt.Println("Katta son:", max)
+
+	//7
+
+	// var num1, num2, num3 int
+
+	// fmt.Print("1-sonni kiriting: ")
+	// fmt.Scan(&num1)
+
+	// fmt.Print("2-sonni kiriting: ")
+	// fmt.Scan(&num2)
+
+	// fmt.Print("3-sonni kiriting: ")
+	// fmt.Scan(&num3)
+
+	// sum1 := num1 + num2
+	// sum2 := num2 + num3
+	// sum3 := num1 + num3
+
+	// max := sum1
+
+	// if sum2 > max {
+	// 	max = sum2
+	// }
+
+	// if sum3 > max {
+	// 	max = sum3
+	// }
+
+	// if sum1 == max {
+	// 	fmt.Println(num1, "+", num2," yigindisi eng katasi ")
+	// }
+
+	// if sum2 == max {
+	// 	fmt.Println(num2, "+", num3," yigindisi eng katasi")
+	// }
+
+	// if sum3 == max {
+	// 	fmt.Println(num1, "+", num3," yigindisi eng katasi")
+	//}
+
+	//8
+
+	// var A, B, C float64
+
+	// fmt.Print("A ni kiriting: ")
+	// fmt.Scan(&A)
+
+	// fmt.Print("B ni kiriting: ")
+	// fmt.Scan(&B)
+
+	// fmt.Print("C ni kiriting: ")
+	// fmt.Scan(&C)
+
+	// if A < B && B < C {
+	// 	A *= 2
+	// 	B *= 2
+	// 	C *= 2
+	// } else {
+	// 	A = -A
+	// 	B = -B
+	// 	C = -C
+	// }
+
+	// fmt.Println("A =", A)
+	// fmt.Println("B =", B)
+	// fmt.Println("C =", C)
+
+	//9
+
+	// var A, B, C float64
+
+	// fmt.Print("A ni kiriting: ")
+	// fmt.Scan(&A)
+
+	// fmt.Print("B ni kiriting: ")
+	// fmt.Scan(&B)
+
+	// fmt.Print("C ni kiriting: ")
+	// fmt.Scan(&C)
+
+	// if (A < B && B < C) || (A > B && B > C) {
+	// 	A *= 2
+	// 	B *= 2
+	// 	C *= 2
+	// } else {
+	// 	A = -A
+	// 	B = -B
+	// 	C = -C
+	// }
+
+	// fmt.Println("A =", A)
+	// fmt.Println("B =", B)
+	// fmt.Println("C =", C)
+
+	//10
+
+	// var num1, num2, num3 int
+
+	// fmt.Print("1-sonni kiriting: ")
+	// fmt.Scan(&num1)
+
+	// fmt.Print("2-sonni kiriting: ")
+	// fmt.Scan(&num2)
+
+	// fmt.Print("3-sonni kiriting: ")
+	// fmt.Scan(&num3)
+
+	// if num1 == num2 {
+	// 	fmt.Println("Tartib raqami:", num3)
+	// } else if num1 == num3 {
+	// 	fmt.Println("Tartib raqami:", num2)
+	// } else if num2 == num3 {
+	// 	fmt.Println("Tartib raqami:", num1)
+	// } else {
+	// 	fmt.Println("Teng sonlar yo'q")
+
+	//11
+
+	// var num1, num2, num3, num4 int
+
+	// fmt.Print("1-sonni kiriting: ")
+	// fmt.Scan(&num1)
+
+	// fmt.Print("2-sonni kiriting: ")
+	// fmt.Scan(&num2)
+
+	// fmt.Print("3-sonni kiriting: ")
+	// fmt.Scan(&num3)
+
+	// fmt.Print("4-sonni kiriting: ")
+	// fmt.Scan(&num4)
+
+	// if num1 == num2 && num2 == num3 {
+	// 	fmt.Println("Tartib raqami:", num4)
+	// } else if num1 == num2 && num2 == num4 {
+	// 	fmt.Println("Tartib raqami:", num3)
+	// } else if num1 == num3 && num3 == num4 {
+	// 	fmt.Println("Tartib raqami:", num2)
+	// } else if num2 == num3 && num3 == num4 {
+	// 	fmt.Println("Tartib raqami:", num1)
+	// } else {
+	// 	fmt.Println("Teng sonlar yo'q")
+	// }
+
+	//                   for
+
+	//1
+
+	// for i := 2; i <= 10; i += 2 {
+	// 	fmt.Println(i)
+	// }
+
+	//2
+	// for i := 1; i <= 10; i += 2 {
+	//     fmt.Println(i)
+	// }
+
+	//3
+
+	// for i := 10; i < 100; i++ {
+	//     if isPrime(i) {
+	//         fmt.Println(i)
+	//     }
+	// }
+
+	//4
+
+	// for i := 100; i < 1000; i++ {
+	//     numStr := fmt.Sprintf("%d", i)
+	//     if numStr[0] == numStr[1] || numStr[0] == numStr[2] || numStr[1] == numStr[2] {
+	//         fmt.Println(i)
+	//     }
+	// }
+
+	//5
+
+	// var num int
+	// fmt.Print("Sonni kiriting: ")
+	// fmt.Scan(&num)
+
+	// digits := []int{}
+	// for num > 0 {
+	//     digits = append(digits, num%10)
+	//     num /= 10
+	// }
+
+	// for i := len(digits) - 1; i >= 0; i-- {
+	//     fmt.Print(digits[i], " ")
+	// }
+
+	//6
+
+	// var n int
+	// fmt.Print("n : ")
+	// fmt.Scan(&n)
+
+	// for i := 1; i <= n; i++ {
+	//     for j := 1; j <= i; j++ {
+	//         fmt.Print("*")
+	//     }
+	//     fmt.Println()
+	// }
+
+	//7
+
+	// var n int
+	// fmt.Print("n : ")
+	// fmt.Scan(&n)
+
+	// for i := 0; i < n; i++ {
+	// 	for j := 0; j < n; j++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	//                   switch
+
+	//1
+	// var Y string
+	// var K int
+
+	// fmt.Print("Robotning boshlang'ich yo'nalishi (s/j/q/g): ")
+	// fmt.Scan(&Y)
+
+	// fmt.Print("Kamanda (0/1/2): ")
+	// fmt.Scan(&K)
+
+	// switch Y {
+	// case "s":
+	// 	switch K {
+	// 	case 0:
+	// 		fmt.Println("Robot shimolda harakat qiladi.")
+	// 	case 1:
+	// 		fmt.Println("Robot chapga buriladi.")
+	// 	case 2:
+	// 		fmt.Println("Robot o'ngga buriladi.")
+	// 	default:
+	// 		fmt.Println("Noto'g'ri kamanda kiritildi.")
+	// 	}
+	// case "j":
+	// 	switch K {
+	// 	case 0:
+	// 		fmt.Println("Robot janubda harakat qiladi.")
+	// 	case 1:
+	// 		fmt.Println("Robot chapga buriladi.")
+	// 	case 2:
+	// 		fmt.Println("Robot o'ngga buriladi.")
+	// 	default:
+	// 		fmt.Println("Noto'g'ri kamanda kiritildi.")
+	// 	}
+	// case "q":
+	// 	switch K {
+	// 	case 0:
+	// 		fmt.Println("Robot sharoda harakat qiladi.")
+	// 	case 1:
+	// 		fmt.Println("Robot chapga buriladi.")
+	// 	case 2:
+	// 		fmt.Println("Robot o'ngga buriladi.")
+	// 	default:
+	// 		fmt.Println("Noto'g'ri kamanda kiritildi.")
+	// 	}
+	// case "g":
+	// 	switch K {
+	// 	case 0:
+	// 		fmt.Println("Robot g'arbda harakat qiladi.")
+	// 	case 1:
+	// 		fmt.Println("Robot chapga buriladi.")
+	// 	case 2:
+	// 		fmt.Println("Robot o'ngga buriladi.")
+	// 	default:
+	// 		fmt.Println("Noto'g'ri kamanda kiritildi.")
+	// 	}
+	// default:
+	// 	fmt.Println("Noto'g'ri yo'nalish kiritildi.")
+	// }
+
+	//2
+
+	// var C string
+	// var K1, K2 int
+
+	// fmt.Print("Lokatorning boshlang'ich holati (s/j/g/g): ")
+	// fmt.Scan(&C)
+
+	// fmt.Print("Kamanda 1 (0/1/2): ")
+	// fmt.Scan(&K1)
+
+	// fmt.Print("Kamanda 2 (0/1/2): ")
+	// fmt.Scan(&K2)
+
+	// switch C {
+	// case "s":
+	// 	switch K1 {
+	// 	case 0:
+	// 		fmt.Println("Lokator shimolda harakat qiladi.")
+	// 	case 1:
+	// 		fmt.Println("Lokator chapga buriladi.")
+	// 	case 2:
+	// 		fmt.Println("Lokator 180 daraja buriladi.")
+	// 	default:
+	// 		fmt.Println("Noto'g'ri kamanda 1 kiritildi.")
+	// 	}
+	// case "j":
+	// 	switch K1 {
+	// 	case 0:
+	// 		fmt.Println("Lokator janubda harakat qiladi.")
+	// 	case 1:
+	// 		fmt.Println("Lokator chapga buriladi.")
+	// 	case 2:
+	// 		fmt.Println("Lokator 180 daraja buriladi.")
+	// 	default:
+	// 		fmt.Println("Noto'g'ri kamanda 1 kiritildi.")
+	// 	}
+	// case "q":
+	// 	switch K1 {
+	// 	case 0:
+	// 		fmt.Println("Lokator sharoda harakat qiladi.")
+	// 	case 1:
+	// 		fmt.Println("Lokator chapga buriladi.")
+	// 	case 2:
+	// 		fmt.Println("Lokator 180 daraja buriladi.")
+	// 	default:
+	// 		fmt.Println("Noto'g'ri kamanda 1 kiritildi.")
+	// 	}
+	// case "g":
+	// 	switch K1 {
+	// 	case 0:
+	// 		fmt.Println("Lokator g'arbda harakat qiladi.")
+	// 	case 1:
+	// 		fmt.Println("Lokator chapga buriladi.")
+	// 	case 2:
+	// 		fmt.Println("Lokator 180 daraja buriladi.")
+	// 	default:
+	// 		fmt.Println("Noto'g'ri kamanda 1 kiritildi.")
+	// 	}
+	// default:
+	// 	fmt.Println("Noto'g'ri boshlang'ich holat kiritildi.")
+	// }
+
+	// switch K2 {
+	// case 0:
+	// 	fmt.Println("Lokator 180 daraja buriladi.")
+	// case 1:
+	// 	fmt.Println("Lokator chapga buriladi.")
+	// case 2:
+	// 	fmt.Println("Lokator o'ngga buriladi.")
+	// default:
+	// 	fmt.Println("Noto'g'ri kamanda 2 kiritildi.")
+	// }
+
+}
+
+// for ni 3 chi massalasi uchun yozilgan funksiya
+// func isPrime(n int) bool {
+// 	if n <= 1 {
+// 		return false
+// 	}
+// 	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+// 		if n%i == 0 {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
